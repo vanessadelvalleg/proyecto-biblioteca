fac6a9a34b0689db043c90212812f1fdc4f1c78a
VanessaDVG <vvalle@giro26.com>
2025-10-09 16:32:11 -0600

    Se actualiza archivo vite para estilos

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost'
-        }
+  server: {
+    host: '0.0.0.0', 
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'host.docker.internal',
+      protocol: 'ws',
     },
+  },
 });
